35074ef53e97ce85cdfdfaabc176fd028d56e43b
Cassiano-Behemonth <oliveira.cassiano@escola.pr.gov.br>
2025-11-05 20:45:01 -0300

    ajuste de gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,5 +20,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Geometric_Neon_Runner"
+rootProject.name = "NeonTunnelRunner"
 include(":app")
